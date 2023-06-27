fe869b149e9f121430a5575410e09317492889fb
lucianommartins <l22145370+lucianommartins@users.noreply.github.com>
2023-06-27 12:42:25 -0300

    adding cloudbuild api to enable_apis.sh

--- enable_apis.sh
+++ enable_apis.sh
@@ -1,3 +1,4 @@
 gcloud services enable translate.googleapis.com
 gcloud services enable dialogflow.googleapis.com
 gcloud services enable run.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
